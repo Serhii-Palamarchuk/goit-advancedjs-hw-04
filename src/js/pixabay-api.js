ae352d411495383ca43654868049116e631b2cf2
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-03-10 21:50:19 +0200

    Enhance fetchImages function to support pagination with page and perPage parameters

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,7 @@ const API_KEY = '49211573-98fed09fb52bd65ccaa252580';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
+export async function fetchImages(query, page = 1, perPage = 3) {
   try {
     const response = await axios.get('', {
       params: {
@@ -13,6 +13,8 @@ export async function fetchImages(query) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        page: page,
+        per_page: perPage,
       },
     });
 
